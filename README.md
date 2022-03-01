14e3c859eea210c3b7f9795a7b4008df06c35a3c
psotoarenas <paola.soto-arenas@uantwerpen.be>
2022-03-01 15:49:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# IoT Lab session: Data analytics and machine learning in the cloud and edge for IoT
+
+This repository is created for the IoT lab session on Data analytics and machine learning in the cloud and edge for IoT. It is composed from two session. During the first session, the students will have a brief introduction to cloud computing and how to containarize applications to be deployed in the cloud and in the edge. During the second session, the students will have also a brief introduction to machine learning, how to use pre-trained models for computer vision problems, and how to train models from a time-series data to forecast future values. 
+
+## Containarized applications. 
+The modules used during this session are within the folders helloworld and covid19_cases. During the theoretical part of the session, we will follow the getting started tutorial directly from docker. For that, it is highly recommended that the students have docker already installed and create a free account on docker hub. Then we will head to developing our first containarized application. The first example (helloworld) shows the differences between running your application locally at your computer and running an application inside a docker container. It is a really simple application but it allows to demonstrate the initial steps of developing application for the cloud. 
+
+The second application (covid19_cases) is a dashboard-like application that displays some figures of the current situation of the covid19 pandemic in different countries around the world. This application is more complex than the first, but it follows the same patterns, showing the students the benefits of containers. This application also can be useful towards the final project. 
+
+## Machine Learning
+[To be completed]
+
+
+## Useful links
+
+### Cloud Computing
+1. Google. Google Cloud Topics: Learn the basics of cloud computing and how to get started. [https://cloud.google.com/learn 
+](https://cloud.google.com/learn 
+). Accessed: 01/03/2022.
+2. AWS Public Sector. Cloud 101: An Introduction to Cloud Computing | AWS Public Sector. [https://youtu.be/GneIpdOirZY](https://youtu.be/GneIpdOirZY). Accessed: 01/03/2022.
+3. TechWorld with Nana. Kubernetes Tutorial for Beginners [FULL COURSE in 4 Hours]. [https://youtu.be/X48VuDVv0do](https://youtu.be/X48VuDVv0do). Accessed: 01/03/2022.
+
+### Containarized Applications
+1. Docker. Docker Getting Started Tutorial. [https://github.com/docker/getting-started](https://github.com/docker/getting-started). Accessed: 01/03/2022. 
+2. Docker. Docker for beginners [https://github.com/docker/labs/blob/master/beginner/readme.md](https://github.com/docker/labs/blob/master/beginner/readme.md). Accessed: 01/03/2022. --> Homework!
+3. Mahbub Zaman. 12 Essential Docker Commands You Should Know. [https://towardsdatascience.com/12-essential-docker-commands-you-should-know-c2d5a7751bb5](https://towardsdatascience.com/12-essential-docker-commands-you-should-know-c2d5a7751bb5). Accessed: 01/03/2022.
+
+### Python Application
+1. Meinhard Ploner. Visualise COVID-19 case data using Python, Dash, and Plotly. [https://towardsdatascience.com/visualise-covid-19-case-data-using-python-dash-and-plotly-e58feb34f70f](https://towardsdatascience.com/visualise-covid-19-case-data-using-python-dash-and-plotly-e58feb34f70f). Accessed: 01/03/2022.
+2. Bee Guan Teo. Building A Real-time Dashboard Using Python Plotly Library And Web Service. [https://towardsdatascience.com/building-a-real-time-dashboard-using-python-plotly-library-and-web-service-145f50d204f0](https://towardsdatascience.com/building-a-real-time-dashboard-using-python-plotly-library-and-web-service-145f50d204f0). Accessed: 01/03/2022.
+3. Anmol Tomar. Dash for Beginners : Python Dashboards. [https://towardsdatascience.com/dash-for-beginners-create-interactive-python-dashboards-338bfcb6ffa4](https://towardsdatascience.com/dash-for-beginners-create-interactive-python-dashboards-338bfcb6ffa4). Accessed: 01/03/2022.
+4. World Health Organization. WHO Coronavirus (COVID-19) Dashboard. [https://covid19.who.int/](https://covid19.who.int/). Accessed: 01/03/2022.
---
+++ covid19_cases/covid_app.py
@@ -0,0 +1,184 @@
+# import all dependencies
+from cgitb import text
+from textwrap import fill
+import dash
+from dash import dcc
+from dash import html
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+from dash.dependencies import Input, Output
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from os.path import isfile
+
+# Auxiliary functions
+
+def get_data(url):
+    # Read data from a URL
+    return pd.read_csv(url)
+
+def get_daily_data():
+    return
+
+def plot_cases(data, country):
+    cases_per_country = data[data["Country"]==country] 
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x = cases_per_country['Date_reported'],
+        y = cases_per_country['New_cases'],
+        line = dict(color = 'firebrick', width = 4),
+        name = 'daily_cases'
+        )
+    )
+    fig.update_layout(title = 'Daily cases over time',
+                      xaxis_title = 'Dates',
+                      yaxis_title = 'Cases'
+                      )
+    return fig
+
+def plot_cum_cases(data, country):
+    cases_per_country = data[data["Country"]==country] 
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x = cases_per_country['Date_reported'],
+        y = cases_per_country['Cumulative_cases'],
+        line = dict(color = 'firebrick', width = 4),
+        fill = 'tozeroy'
+        )
+    )
+    fig.update_layout(title = 'Cumulated cases over time',
+                      xaxis_title = 'Dates',
+                      yaxis_title = 'Cases'
+                      )
+    return fig
+
+def geo_summary(data):
+    last_day_reported = data['Date_reported'].iloc[-1]
+    last_report_overall = data[data['Date_reported']==last_day_reported]
+    fig = px.scatter_geo(
+        last_report_overall, 
+        locations='Country', 
+        locationmode='country names', 
+        color='WHO_region', 
+        hover_name='Country', 
+        size='Cumulative_cases', 
+        projection='natural earth'
+        )
+    fig.update_layout(
+        title = f'Global COVID-19 Cases (Last Updated: {last_day_reported})'
+        ),
+    return fig
+
+def top_countries(cases_data, vaccination_data):
+    last_day_reported = cases_data['Date_reported'].iloc[-1]
+    last_report_overall = cases_data[cases_data['Date_reported']==last_day_reported]
+    fig = make_subplots(
+    rows = 3, cols = 1,
+    # specs=[
+    #         [    None, None, None,               {"type": "bar", "colspan":1}, None, None],
+    #         [    None, None, None,              {"type": "bar", "colspan":1}, None, None],
+    #         [    None, None, None,               {"type": "bar", "colspan":1}, None, None],
+    #       ]
+    )
+
+    # Top 10 Countries with recently discovered cases
+    top_ten_cases_daily = last_report_overall.nlargest(n=10, columns=['New_cases'])
+    fig.append_trace(go.Bar(
+        x=top_ten_cases_daily['Country_code'],
+        y=top_ten_cases_daily['New_cases'],
+        name= "Confirmed Cases",
+        ), 
+        row=1, col=1
+    )
+    
+    # Top 10 Countries with highest daily deaths
+    top_ten_deaths_daily = last_report_overall.nlargest(n=10, columns=['New_deaths'])
+    fig.append_trace(go.Bar(
+        x=top_ten_deaths_daily['Country_code'],
+        y=top_ten_deaths_daily['New_deaths'],
+        name= "Confirmed Deaths",
+        ), 
+        row=2, col=1
+    )
+    
+    # Top 10 Countries with highest number of people vaccinated
+    top_ten_vaccinated = vaccination_data.nlargest(n=10, columns=['PERSONS_FULLY_VACCINATED'])
+    fig.append_trace(go.Bar(
+        x=top_ten_vaccinated['COUNTRY'],
+        y=top_ten_vaccinated['PERSONS_FULLY_VACCINATED'],
+        name= "Fully Vaccinated",
+        ), 
+        row=3, col=1
+    )
+    
+    fig.update_layout(
+        title_text=f"Top 10 Countries in daily cifers (Last Updated: {last_day_reported})",
+        )
+
+    return fig
+
+
+#  official WHO data
+cases_who_url = 'https://covid19.who.int/WHO-COVID-19-global-data.csv'
+vaccination_who_url = 'https://covid19.who.int/who-data/vaccination-data.csv'
+
+cases = get_data(cases_who_url)
+vaccination = get_data(vaccination_who_url)
+
+
+
+countries = cases["Country"].unique()
+countries.sort()
+
+
+
+# Visualization
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)   
+
+app.layout = html.Div(
+    id = 'parent', 
+    children = [
+        html.H1(id = 'H1', 
+        children = 'Coronavirus (COVID-19) Data', 
+        style = {'textAlign':'center',} # 'marginTop':40,'marginBottom':40
+        ),
+        html.H3('Overall Numbers'),
+        html.Div(children=[
+            html.H5('Cumulated Cases'),
+            dcc.Graph(id='geo_summary', figure=geo_summary(cases))
+        ],
+        style={'width': '49%', 'display': 'inline-block'}),
+        html.Div(children=[
+            html.H5('Daily Cases'),
+            dcc.Graph(id='top_countries', figure=top_countries(cases, vaccination))
+        ],
+        style={'width': '49%', 'display': 'inline-block'}),            
+        
+        html.H5('Country'),
+        dcc.Dropdown(
+            id='country',
+            options=[{'label':c, 'value':c} for c in countries],
+            value='Belgium'
+        ),
+        dcc.Graph(id='daily_cases'),
+        dcc.Graph(id='cum_cases')   
+    ]
+)
+
+@app.callback(
+    Output(component_id='daily_cases', component_property='figure'),
+    Output(component_id='cum_cases', component_property='figure'),
+    Input(component_id='country', component_property='value')
+)
+def update_plots(country):
+    # Function for creating figures showing per country daily and cumulated cases over time
+    data = get_data(cases_who_url)
+    daily_fig = plot_cases(data=data, country=country)
+    cum_fig = plot_cum_cases(data=data, country=country)
+    return daily_fig, cum_fig
+
+if __name__ == '__main__':
+    app.run_server(host="0.0.0.0", debug=True) # opens a port on http://0.0.0.0:8050/
